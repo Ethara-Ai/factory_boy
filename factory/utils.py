d1545a9a436ae35ca48963550fdfec424a1fd480
prathamshukla11111 <prathamshukla11111@users.noreply.github.com>
2026-04-05 12:42:00 +0000

    Fix more import-time stubs: sort_ordered_objects, get_builder_phase, touch_creation_counter

--- factory/utils.py
+++ factory/utils.py
@@ -21,7 +21,8 @@ class log_pprint:
     Since it is only used with factory.debug(), the computation is
     performed lazily.
     """
-    __slots__ = ['args', 'kwargs']
+
+    __slots__ = ["args", "kwargs"]
 
     def __init__(self, args=(), kwargs=None):
         self.args = args
@@ -31,18 +32,15 @@ class log_pprint:
         return repr(str(self))
 
     def __str__(self):
-        return ', '.join(
-            [
-                repr(arg) for arg in self.args
-            ] + [
-                '%s=%s' % (key, repr(value))
-                for key, value in self.kwargs.items()
-            ]
+        return ", ".join(
+            [repr(arg) for arg in self.args]
+            + ["%s=%s" % (key, repr(value)) for key, value in self.kwargs.items()]
         )
 
 
 class ResetableIterator:
     """An iterator wrapper that can be 'reset()' to its start."""
+
     def __init__(self, iterator, **kwargs):
         super().__init__(**kwargs)
         self.iterator = iter(iterator)
@@ -72,7 +70,7 @@ class OrderedBase:
     Each instance (even from subclasses) will share a global creation counter.
     """
 
-    CREATION_COUNTER_FIELD = '_creation_counter'
+    CREATION_COUNTER_FIELD = "_creation_counter"
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -80,7 +78,13 @@ class OrderedBase:
             self.touch_creation_counter()
 
     def touch_creation_counter(self):
-        pass
+        bases = type(self).__mro__
+        root = bases[bases.index(OrderedBase) - 1]
+        if not hasattr(root, self.CREATION_COUNTER_FIELD):
+            setattr(root, self.CREATION_COUNTER_FIELD, 0)
+        next_counter = getattr(root, self.CREATION_COUNTER_FIELD)
+        setattr(self, self.CREATION_COUNTER_FIELD, next_counter)
+        setattr(root, self.CREATION_COUNTER_FIELD, next_counter + 1)
 
 
 def sort_ordered_objects(items, getter=lambda x: x):
@@ -94,4 +98,6 @@ def sort_ordered_objects(items, getter=lambda x: x):
         >>> sort_ordered_objects([x, y, z])
         >>> sort_ordered_objects(v.items(), getter=lambda e: e[1])
     """
-    pass
+    return sorted(
+        items, key=lambda x: getattr(getter(x), OrderedBase.CREATION_COUNTER_FIELD, -1)
+    )
