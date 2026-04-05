98b93b713afa8743a97e640f97323717df4706ba
prathamshukla11111 <prathamshukla11111@users.noreply.github.com>
2026-04-05 12:39:14 +0000

    Fix import-time stubs: restore FactoryOptions, resolve_attribute, get_factory_bases, OptionDefault.apply, _Counter

--- factory/base.py
+++ factory/base.py
@@ -8,21 +8,24 @@ from typing import Generic, List, Type, TypeVar
 
 from . import builder, declarations, enums, errors, utils
 
-logger = logging.getLogger('factory.generate')
+logger = logging.getLogger("factory.generate")
 
-T = TypeVar('T')
+T = TypeVar("T")
 
 # Factory metaclasses
 
 
 def get_factory_bases(bases):
     """Retrieve all FactoryMetaClass-derived bases from a list."""
-    pass
+    return [b for b in bases if issubclass(b, BaseFactory)]
 
 
 def resolve_attribute(name, bases, default=None):
     """Find the first definition of an attribute according to MRO order."""
-    pass
+    for base in bases:
+        if hasattr(base, name):
+            return getattr(base, name)
+    return default
 
 
 class FactoryMetaClass(type):
@@ -41,8 +44,9 @@ class FactoryMetaClass(type):
         elif cls._meta.strategy == enums.STUB_STRATEGY:
             return cls.stub(**kwargs)
         else:
-            raise errors.UnknownStrategy('Unknown Meta.strategy: {}'.format(
-                cls._meta.strategy))
+            raise errors.UnknownStrategy(
+                "Unknown Meta.strategy: {}".format(cls._meta.strategy)
+            )
 
     def __new__(mcs, class_name, bases, attrs):
         """Record attributes as a pattern for later instance construction.
@@ -65,17 +69,16 @@ class FactoryMetaClass(type):
         else:
             base_factory = None
 
-        attrs_meta = attrs.pop('Meta', None)
-        attrs_params = attrs.pop('Params', None)
+        attrs_meta = attrs.pop("Meta", None)
+        attrs_params = attrs.pop("Params", None)
 
-        base_meta = resolve_attribute('_meta', bases)
-        options_class = resolve_attribute('_options_class', bases, FactoryOptions)
+        base_meta = resolve_attribute("_meta", bases)
+        options_class = resolve_attribute("_options_class", bases, FactoryOptions)
 
         meta = options_class()
-        attrs['_meta'] = meta
+        attrs["_meta"] = meta
 
-        new_class = super().__new__(
-            mcs, class_name, bases, attrs)
+        new_class = super().__new__(mcs, class_name, bases, attrs)
 
         meta.contribute_to_class(
             new_class,
@@ -89,9 +92,9 @@ class FactoryMetaClass(type):
 
     def __str__(cls):
         if cls._meta.abstract:
-            return '<%s (abstract)>' % cls.__name__
+            return "<%s (abstract)>" % cls.__name__
         else:
-            return f'<{cls.__name__} for {cls._meta.model}>'
+            return f"<{cls.__name__} for {cls._meta.model}>"
 
 
 class BaseMeta:
@@ -110,6 +113,7 @@ class OptionDefault:
         checker: callable or None, an optional function used to detect invalid option
             values at declaration time
     """
+
     def __init__(self, name, value, inherit=False, checker=None):
         self.name = name
         self.value = value
@@ -117,12 +121,24 @@ class OptionDefault:
         self.checker = checker
 
     def apply(self, meta, base_meta):
-        pass
+        value = self.value
+        if meta is not None and hasattr(meta, self.name):
+            value = getattr(meta, self.name)
+        elif self.inherit and base_meta is not None and hasattr(base_meta, self.name):
+            value = getattr(base_meta, self.name)
+
+        if self.checker is not None:
+            self.checker(meta, value)
+
+        return value
 
     def __str__(self):
-        return '%s(%r, %r, inherit=%r)' % (
+        return "%s(%r, %r, inherit=%r)" % (
             self.__class__.__name__,
-            self.name, self.value, self.inherit)
+            self.name,
+            self.value,
+            self.inherit,
+        )
 
 
 class FactoryOptions:
@@ -140,27 +156,105 @@ class FactoryOptions:
 
     @property
     def declarations(self):
-        pass
+        base_declarations = dict(self.base_declarations)
+        for name, param in utils.sort_ordered_objects(
+            self.parameters.items(), getter=lambda item: item[1]
+        ):
+            base_declarations.update(param.as_declarations(name, base_declarations))
+        return base_declarations
 
     def _build_default_options(self):
-        """"Provide the default value for all allowed fields.
+        """ "Provide the default value for all allowed fields.
 
         Custom FactoryOptions classes should override this method
         to update() its return value.
         """
-        pass
+
+        def is_model(meta, value):
+            if isinstance(value, FactoryMetaClass):
+                raise TypeError("%s is already a %s" % (repr(value), Factory.__name__))
+
+        return [
+            OptionDefault("model", None, inherit=True, checker=is_model),
+            OptionDefault("abstract", False, inherit=False),
+            OptionDefault("strategy", enums.CREATE_STRATEGY, inherit=True),
+            OptionDefault("inline_args", (), inherit=True),
+            OptionDefault("exclude", (), inherit=True),
+            OptionDefault("rename", {}, inherit=True),
+        ]
 
     def _fill_from_meta(self, meta, base_meta):
         # Exclude private/protected fields from the meta
-        pass
+        if meta is None:
+            meta_attrs = {}
+        else:
+            meta_attrs = {
+                k: v for (k, v) in vars(meta).items() if not k.startswith("_")
+            }
 
-    def contribute_to_class(self, factory, meta=None, base_meta=None, base_factory=None, params=None):
+        for option in self._build_default_options():
+            assert not hasattr(self, option.name), (
+                "Can't override field %s." % option.name
+            )
+            value = option.apply(meta, base_meta)
+            meta_attrs.pop(option.name, None)
+            setattr(self, option.name, value)
 
-        pass
+        if meta_attrs:
+            raise TypeError(
+                "'class Meta' for %r got unknown attribute(s) %s"
+                % (self.factory, ",".join(sorted(meta_attrs.keys())))
+            )
+
+    def contribute_to_class(
+        self, factory, meta=None, base_meta=None, base_factory=None, params=None
+    ):
+
+        self.factory = factory
+        self.base_factory = base_factory
+
+        self._fill_from_meta(meta=meta, base_meta=base_meta)
+
+        self.model = self.get_model_class()
+        if self.model is None:
+            self.abstract = True
+
+        self.counter_reference = self._get_counter_reference()
+
+        for parent in reversed(self.factory.__mro__[1:]):
+            if not hasattr(parent, "_meta"):
+                continue
+            self.base_declarations.update(parent._meta.base_declarations)
+            self.parameters.update(parent._meta.parameters)
+
+        for k, v in vars(self.factory).items():
+            if self._is_declaration(k, v):
+                self.base_declarations[k] = v
+
+        if params is not None:
+            for k, v in utils.sort_ordered_objects(
+                vars(params).items(), getter=lambda item: item[1]
+            ):
+                if not k.startswith("_"):
+                    self.parameters[k] = declarations.SimpleParameter.wrap(v)
+
+        self._check_parameter_dependencies(self.parameters)
+
+        self.pre_declarations, self.post_declarations = builder.parse_declarations(
+            self.declarations
+        )
 
     def _get_counter_reference(self):
         """Identify which factory should be used for a shared counter."""
-        pass
+        if (
+            self.model is not None
+            and self.base_factory is not None
+            and self.base_factory._meta.model is not None
+            and issubclass(self.model, self.base_factory._meta.model)
+        ):
+            return self.base_factory._meta.counter_reference
+        else:
+            return self
 
     def _initialize_counter(self):
         """Initialize our counter pointer.
@@ -168,7 +262,14 @@ class FactoryOptions:
         If we're the top-level factory, instantiate a new counter
         Otherwise, point to the top-level factory's counter.
         """
-        pass
+        if self._counter is not None:
+            return
+
+        if self.counter_reference is self:
+            self._counter = _Counter(seq=self.factory._setup_next_sequence())
+        else:
+            self.counter_reference._initialize_counter()
+            self._counter = self.counter_reference._counter
 
     def next_sequence(self):
         """Retrieve a new sequence ID.
@@ -178,20 +279,60 @@ class FactoryOptions:
         - _setup_next_sequence, if this is the 'toplevel' factory and the
             sequence counter wasn't initialized yet; then increase it.
         """
-        pass
+        self._initialize_counter()
+        return self._counter.next()
 
     def reset_sequence(self, value=None, force=False):
-        pass
+        self._initialize_counter()
+
+        if self.counter_reference is not self and not force:
+            raise ValueError(
+                "Can't reset a sequence on descendant factory %r; reset sequence on %r or use `force=True`."
+                % (self.factory, self.counter_reference.factory)
+            )
+
+        if value is None:
+            value = self.counter_reference.factory._setup_next_sequence()
+        self._counter.reset(value)
 
     def prepare_arguments(self, attributes):
         """Convert an attributes dict to a (args, kwargs) tuple."""
-        pass
+        kwargs = dict(attributes)
+        kwargs = self.factory._adjust_kwargs(**kwargs)
+
+        kwargs = {
+            k: v
+            for k, v in kwargs.items()
+            if k not in self.exclude
+            and k not in self.parameters
+            and v is not declarations.SKIP
+        }
+
+        for old_name, new_name in self.rename.items():
+            if old_name in kwargs:
+                kwargs[new_name] = kwargs.pop(old_name)
+
+        args = tuple(kwargs.pop(arg_name) for arg_name in self.inline_args)
+
+        return args, kwargs
 
     def instantiate(self, step, args, kwargs):
-        pass
+        model = self.get_model_class()
+
+        if step.builder.strategy == enums.BUILD_STRATEGY:
+            return self.factory._build(model, *args, **kwargs)
+        elif step.builder.strategy == enums.CREATE_STRATEGY:
+            return self.factory._create(model, *args, **kwargs)
+        else:
+            assert step.builder.strategy == enums.STUB_STRATEGY
+            return StubObject(**kwargs)
 
     def use_postgeneration_results(self, step, instance, results):
-        pass
+        self.factory._after_postgeneration(
+            instance,
+            create=step.builder.strategy == enums.CREATE_STRATEGY,
+            results=results,
+        )
 
     def _is_declaration(self, name, value):
         """Determines if a class attribute is a field value declaration.
@@ -201,11 +342,38 @@ class FactoryOptions:
         is private (name starts with '_') or a classmethod or staticmethod.
 
         """
-        pass
+        if isinstance(value, (classmethod, staticmethod)):
+            return False
+        elif enums.get_builder_phase(value):
+            return True
+        return not name.startswith("_")
 
     def _check_parameter_dependencies(self, parameters):
         """Find out in what order parameters should be called."""
-        pass
+        deep_revdeps = collections.defaultdict(set)
+        deps = collections.defaultdict(set)
+
+        for name, parameter in parameters.items():
+            if isinstance(parameter, declarations.Parameter):
+                field_revdeps = parameter.get_revdeps(parameters)
+                if not field_revdeps:
+                    continue
+                deep_revdeps[name] = set.union(
+                    *(deep_revdeps[dep] for dep in field_revdeps)
+                )
+                deep_revdeps[name] |= set(field_revdeps)
+                for dep in field_revdeps:
+                    deps[dep].add(name)
+
+        cyclic = [
+            name for name, field_deps in deep_revdeps.items() if name in field_deps
+        ]
+        if cyclic:
+            raise errors.CyclicDefinitionError(
+                "Cyclic definition detected on %r; Params around %s"
+                % (self.factory, ", ".join(cyclic))
+            )
+        return deps
 
     def get_model_class(self):
         """Extension point for loading model classes.
@@ -213,7 +381,7 @@ class FactoryOptions:
         This can be overridden in framework-specific subclasses to hook into
         existing model repositories, for instance.
         """
-        pass
+        return self.model
 
     def __str__(self):
         return "<%s for %s>" % (self.__class__.__name__, self.factory.__name__)
@@ -237,10 +405,12 @@ class _Counter:
         self.seq = seq
 
     def next(self):
-        pass
+        value = self.seq
+        self.seq += 1
+        return value
 
     def reset(self, next_value=0):
-        pass
+        self.seq = next_value
 
 
 class BaseFactory(Generic[T]):
@@ -252,7 +422,7 @@ class BaseFactory(Generic[T]):
 
     def __new__(cls, *args, **kwargs):
         """Would be called if trying to instantiate the class."""
-        raise errors.FactoryError('You cannot instantiate BaseFactory')
+        raise errors.FactoryError("You cannot instantiate BaseFactory")
 
     _meta = FactoryOptions()
 
@@ -483,13 +653,13 @@ Factory.AssociatedClassError = errors.AssociatedClassError
 
 class StubObject:
     """A generic container."""
+
     def __init__(self, **kwargs):
         for field, value in kwargs.items():
             setattr(self, field, value)
 
 
 class StubFactory(Factory):
-
     class Meta:
         strategy = enums.STUB_STRATEGY
         model = StubObject
@@ -505,6 +675,7 @@ class StubFactory(Factory):
 
 class BaseDictFactory(Factory):
     """Factory for dictionary-like classes."""
+
     class Meta:
         abstract = True
 
@@ -524,6 +695,7 @@ class DictFactory(BaseDictFactory):
 
 class BaseListFactory(Factory):
     """Factory for list-like classes."""
+
     class Meta:
         abstract = True
 
